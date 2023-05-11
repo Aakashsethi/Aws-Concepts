56098f4756f0b7836e06db65abbf260960d7a51f
aakashsethi <aakash.sethi7@gmail.com>
2023-05-11 13:06:47 -0400

    initial commit

---
+++ src/main/java/Nosh/NoshReciepie/NoshReciepieApplication.java
@@ -0,0 +1,13 @@
+package Nosh.NoshReciepie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoshReciepieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NoshReciepieApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Nosh/NoshReciepie/NoshReciepieApplicationTests.java
@@ -0,0 +1,13 @@
+package Nosh.NoshReciepie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoshReciepieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
